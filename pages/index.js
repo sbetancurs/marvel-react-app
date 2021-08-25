41ef47fd59e7795bfad7d5959037ac945444669f
sbetancurs <sebasbetancur97@gmail.com>
2021-08-25 00:03:46 -0500

    import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 
-import Characters from "../components/characters";
+import Characters from "../components/Characters";
 
 export default function Home() {
   const [orderBy, setOrderBy] = useState("");
